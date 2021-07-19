5d0a938a8ba9c41a7a476807679397ce4b9379be
Khang Nguyen <knguyen@mapcommunications.com>
2021-07-19 00:10:07 -0400

    Added DSC configuration for managing power settings

---
+++ ComputerManagementTasks/DscResources/PowerSettings/PowerSettings.schema.psm1
@@ -0,0 +1,26 @@
+<#
+    .DESCRIPTION
+        The resource allows you to configure a computer by changing its name and description and modifying its Active Directory domain or workgroup membership.
+#>
+#Requires -Module ComputerManagementDsc
+
+configuration PowerSettings
+{
+    param (
+        [Parameter(Mandatory)]
+        [System.String]
+        $PowerPlan
+    )
+
+    Import-DscResource -ModuleName ComputerManagementDsc
+
+    # create execution name for DSC resource
+    $executionName = "$($PowerPlan)"
+
+    # create DSC resource for Power Plan activation
+    PowerPlan "$executionNamne"
+    {
+        IsSingleInstance = 'Yes'
+        Name             = $PowerPlan
+    }
+}
